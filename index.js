78e9dc3ce28c2c437579e608273ead14187dbf90
Ali Ezadkhaha <hodhod212@yahoo.com>
2020-08-27 04:33:15 +0200

    Created room and username input

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
+const express = require("express");
 var app = require("express")();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
+const fetch = require("node-fetch");
+app.use(express.static("public"));
+const fs = require("fs");
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+var usernames = {};
+var rooms = ["room1", "room2", "room3"];
+io.sockets.on("connection", function (socket) {
+  socket.on("adduser", function (username) {
+    socket.username = username;
+    socket.room = "room1";
+    usernames[username] = username;
+    socket.join("room1");
+    socket.emit("updatechat", "SERVER", "you have connected to room1");
+    socket.broadcast
+      .to("room1")
+      .emit("updatechat", "SERVER", username + " has connected to this room");
+    socket.emit("updaterooms", rooms, "room1");
+  });
+  socket.on("sendchat", function (data) {
+    io.sockets.in(socket.room).emit("updatechat", socket.username, data);
+  });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  socket.on("switchRoom", function (newroom) {
+    socket.leave(socket.room);
+    socket.join(newroom);
+    socket.emit("updatechat", "SERVER", "you have connected to " + newroom);
+    socket.broadcast
+      .to(socket.room)
+      .emit("updatechat", "SERVER", socket.username + " has left this room");
+    socket.room = newroom;
+    socket.broadcast
+      .to(newroom)
+      .emit("updatechat", "SERVER", socket.username + " has joined this room");
+    socket.emit("updaterooms", rooms, newroom);
+  });
+  socket.on("disconnect", function () {
+    delete usernames[socket.username];
+    io.sockets.emit("updateusers", usernames);
+    socket.broadcast.emit(
+      "updatechat",
+      "SERVER",
+      socket.username + " has disconnected"
+    );
+    socket.leave(socket.room);
   });
 });
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
-  });
-});
+
 http.listen(3100, () => {
   console.log("listening on *:3100");
 });
